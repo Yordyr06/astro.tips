0def6fa96d438efd5e5ab61fa9f778097a394d8d
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-24 03:39:24 -0400

    Project Config

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+    plugins: [
+        '@stylistic/js' 
+      ],
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "standard-with-typescript",
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        '@stylistic/js/indent': ['error', 2],
+        '@stylistic/js/semi': "always"
+    }
+}
